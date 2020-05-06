ce92ec53c594830a9ec83371a157e9162b607a9f
tracht <marija.trachtenberg@gmail.com>
2020-05-06 15:31:41 +0100

    refactor

--- spec/features/bookmark_spec.rb
+++ spec/features/bookmark_spec.rb
@@ -2,17 +2,15 @@ require 'pg'
 
 feature 'view bookmark' do
   scenario 'shows all bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    
-# Add the test data
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.destroyallsoftware.com")
+    Bookmark.create(url: "http://www.google.com")
 
-    visit('/')
-    click_button('View bookmarks')
-    expect(page).to have_content "Bookmarks"
+    visit('/bookmarks')
+    # click_button('View bookmarks')
+    # expect(page).to have_content "Bookmarks"
     expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
     expect(page).to have_content "http://www.google.com"
   end
 end
