e27f5d893b09f019d49bc9bdc656a309473ec6d9
mbrad26 <mbrad26@gmail.com>
2020-05-06 16:56:53 +0100

    work in progres step 11

--- spec/bookmark_class_spec.rb
+++ spec/bookmark_class_spec.rb
@@ -6,19 +6,24 @@ describe Bookmark do
     it 'shows all saved bookmarks' do
       connection = PG.connect(dbname: 'bookmark_manager_test')
       # Add the test data
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+      Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
+      Bookmark.create(url: "http://www.google.com", title: "Google")
+
       bookmarks = Bookmark.all
+
       expect(bookmarks).to include("http://www.makersacademy.com")
+      expect(bookmarks).to include('http://www.destroyallsoftware.com')
       expect(bookmarks).to include("http://www.google.com")
     end
   end
 
   describe '.create' do
     it 'creates new bookmark' do
-      Bookmark.create(url: 'http://www.testbookmark.com')
-      expect(Bookmark.all).to include 'http://www.testbookmark.com'
+      Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark').first
+
+      expect(Bookmark.create['url']).to eq 'http://www.testbookmark.com'
+      expect(Bookmark.create['title']).to eq 'Test Bookmark'
     end
   end
 
--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -2,6 +2,7 @@ feature 'Bookmark page' do
   scenario 'Shows add bookmark button' do
     visit('/bookmarks/new')
     fill_in('url', with: 'http://testbookmark.com')
+    fill_in('title', with: 'Test Bookmark')
     click_button('Add Bookmark')
     expect(page).to have_content 'http://testbookmark.com'
   end
